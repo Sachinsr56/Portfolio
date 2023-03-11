e37ceaf4f27a6b45c41476bcbcb0ea8c25de823d
Sachinsr56 <sachinkumarhas@gmail.com>
2023-03-11 22:36:21 +0530

    Project Videos added

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import './Project.css'
+import React from "react";
+import ChatApp from "../Asssets/ChatApp.gif";
+import CryptoHunter from "../Asssets/CryptoHunter.gif";
+import MovieApp from "../Asssets/MovieApp.gif";
+import "./Project.css";
 
 const Project = () => {
   return (
-    <div>Project</div>
-  )
-}
+    <div className="project-section">
+      <div className="container">
+        <div className="heading">Project</div>
+        <div className="row">
+          <div className="card col-md-3 col-sm-12">
+            <img src={CryptoHunter} alt="CryptoHunter" />
+          </div>
+          <div className="card col-md-3 col-sm-12">
+            <img src={MovieApp} alt="MovieApp" />
+          </div>
+          <div className="card col-md-3 col-sm-12">
+            <img src={ChatApp} alt="ChatApp" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Project
+export default Project;
